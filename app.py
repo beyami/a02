4e62d7d2c36a5164a3d53d33c9f1d5bfd8c80769
beyami <108394424+beyami@users.noreply.github.com>
2023-02-25 02:01:19 +0000

    /と/resultの統一

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 
 # Spotify API認証
 # 環境変数の設定方法
@@ -15,48 +14,41 @@ app = Flask(__name__)
 # トップページ
 @app.route("/", methods=["GET", "POST"])
 def search():
+    # "POST"なら検索結果表示
     if request.method == "POST":
-        # 入力内容を取得したらステータスコード307で/suggestへリダイレクト
-        # ↑不通にリダイレクトするとGETに切り替えられてしまうため
-        return redirect("/suggest", 307)
+        # '/' のフォーム内容を取得
+        song_name = str(request.form.get('song_name'))
+
+        # '/' のフォーム内容が与えられていない場合の処理
+        if song_name == None:
+            song_name = ''
+
+        # Spotify API から曲名で検索
+        search_results = spotify.search(q='track:' + song_name, limit=5, offset=0, type='track', market=None).get('tracks').get('items')
+
+        # 検索結果を格納するための配列
+        # 要素は辞書型です
+        songs = []
+
+        # 複数の検索結果それぞれに処理をする
+        for search_result in search_results:
+            # アーティストの名前が複数人いる場合に整形する処理
+            # アーティストの名前を格納するための変数
+            artist_name = ''
+            counter = 0
+            for artist in search_result.get('artists'):
+                if counter == len(search_result.get('artists')) - 1:
+                    artist_name += artist.get('name')
+                else:
+                    artist_name += artist.get('name') + ', '
+                counter += 1
+
+            # 検索結果を辞書型で格納
+            songs.append({'name'    :search_result.get('name'),
+                        'id'      :search_result.get('id'),
+                        'artist'  :artist_name})
+
+        return render_template('suggest.html', songs=songs)
     else:
         # GETならフォームを表示、検索内容取得
         return render_template('index.html')
-
-# 検索候補を表示するページ
-@app.route('/suggest', methods=['POST'])
-def suggest():
-
-    # '/' のフォーム内容を取得
-    song_name = str(request.form.get('song_name'))
-
-    # '/' のフォーム内容が与えられていない場合の処理
-    if song_name == None:
-        song_name = ''
-
-    # Spotify API から曲名で検索
-    search_results = spotify.search(q='track:' + song_name, limit=5, offset=0, type='track', market=None).get('tracks').get('items')
-
-    # 検索結果を格納するための配列
-    # 要素は辞書型です
-    songs = []
-
-    # 複数の検索結果それぞれに処理をする
-    for search_result in search_results:
-        # アーティストの名前が複数人いる場合に整形する処理
-        # アーティストの名前を格納するための変数
-        artist_name = ''
-        counter = 0
-        for artist in search_result.get('artists'):
-            if counter == len(search_result.get('artists')) - 1:
-                artist_name += artist.get('name')
-            else:
-                artist_name += artist.get('name') + ', '
-            counter += 1
-
-        # 検索結果を辞書型で格納
-        songs.append({'name'    :search_result.get('name'),
-                      'id'      :search_result.get('id'),
-                      'artist'  :artist_name})
-
-    return render_template('suggest.html', songs=songs)
