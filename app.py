0c1a94dae7b847e4afa8b6b2b8f319fb8a323d75
glassrumraisin <116626659+glassrumraisin@users.noreply.github.com>
2023-02-25 12:37:49 +0900

    spotipyを利用しない形式に書き換え

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 from flask import Flask, request, render_template
-<<<<<<< HEAD
 import requests
 import json
-=======
-import spotipy
-
-# Spotify API認証
-# 環境変数の設定方法
-# export SPOTIPY_CLIENT_ID="(あなたのSpotify Client ID)"
-# export SPOTIPY_CLIENT_SECRET="(あなたのSpotify Client Secret)"
-# 上記のコマンドをターミナルで実行する
-client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials()
-spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
->>>>>>> 736455d0acb0046a054b2d26ce946aae9bc4647a
 
 app = Flask(__name__)
 
@@ -68,7 +56,6 @@ def search():
     # "POST"なら検索結果表示
     if request.method == "POST":
         # フォーム内容取得
-<<<<<<< HEAD
         song_name = request.form.get('song_name')
 
         # フォーム内容が与えられていない場合の処理
@@ -77,44 +64,8 @@ def search():
 
         # 検索結果を取得
         songs = search_songs(song_name)
-=======
-        song_name = str(request.form.get('song_name'))
-
-        # フォーム内容が与えられていない場合の処理
-        if song_name == None:
-            song_name = ''
-
-        # Spotify API から曲名で検索
-        search_results = spotify.search(q='track:' + song_name, limit=5, offset=0, type='track', market=None).get('tracks').get('items')
-
-        # 検索結果を格納するための配列
-        # 要素は辞書型です
-        songs = []
-
-        # 複数の検索結果それぞれに処理をする
-        for search_result in search_results:
-            # アーティストの名前が複数人いる場合に整形する処理
-            # アーティストの名前を格納するための変数
-            artist_name = ''
-            counter = 0
-            for artist in search_result.get('artists'):
-                if counter == len(search_result.get('artists')) - 1:
-                    artist_name += artist.get('name')
-                else:
-                    artist_name += artist.get('name') + ', '
-                counter += 1
-
-            # 検索結果を辞書型で格納
-            songs.append({'name'    :search_result.get('name'),
-                        'id'      :search_result.get('id'),
-                        'artist'  :artist_name})
->>>>>>> 736455d0acb0046a054b2d26ce946aae9bc4647a
 
         return render_template('suggest.html', songs=songs)
     else:
         # GETならフォームを表示、検索内容取得
-<<<<<<< HEAD
         return render_template('index.html')
-=======
-        return render_template('index.html')
->>>>>>> 736455d0acb0046a054b2d26ce946aae9bc4647a
